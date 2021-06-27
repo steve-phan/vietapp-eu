2e418fbf5f02191d61fc58043cf8ba5ded478031
vUnAmp <phanhaingoc@gmail.com>
2021-06-27 12:03:38 +0200

    page title

--- src/pages/email-marketing.js
+++ src/pages/email-marketing.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 const index = ({ location }) => {
   return (
     <Layout location={location}>
+      <Seo title="Email marketing Online" />
       <h4>Dịch vụ đang được xây dựng</h4>
     </Layout>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/Hero"
 import WhyBooking from "../components/WhyBooking"
+import Seo from "../components/seo"
 
 const index = ({ location }) => {
   return (
     <Layout location={location}>
+      <Seo title="Booking Online System" />
       <Hero />
       <WhyBooking />
     </Layout>
